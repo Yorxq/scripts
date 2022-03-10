8a02afa3e0a2bdcb82d96bdede4db5a83ac03771
Yorqx <88094250+Yorxq@users.noreply.github.com>
2022-03-10 11:26:01 +0100

    Update protect.lua

--- protect.lua
+++ protect.lua
@@ -1,6 +1,6 @@
 local Player = game.Players.LocalPlayer
 local Char = Player.Character
-local protecting
+local loop
 
 
 local function FindPlayer(name)
@@ -23,14 +23,48 @@ Player.CharacterAdded:Connect(function()
 
 		if Command == ";protect" or Command == ";prot" then
 			if Recipient ~= nil then
-				protecting = true
-				while wait(0.01) do
-					local hrp = Char.HumanoidRootPart
-					local target = game.Players:FindFirstChild(Recipient).Character:WaitForChild("Humanoid")
-					hrp.CFrame = target.RootPart.CFrame + target.RootPart.CFrame.LookVector * 20
-				end
+				loop = game:GetService('RunService').Stepped:Connect(function()
+					pcall(function()
+						local hrp = Char.HumanoidRootPart
+						local target = game.Players:FindFirstChild(Recipient).Character:WaitForChild("Humanoid")
+						hrp.CFrame = target.RootPart.CFrame + target.RootPart.CFrame.LookVector * 20
+						
+						if target.Health <= 0 then loop = loop:Disconnect() end
+						if Player.Character:WaitForChild("Humanoid").Health <= 0 then loop = loop:Disconnect() end
+					end)
+				end)
+			end
+		elseif Command == ";unprotect" or Command == ";unprot" or Command == ";stop" then
+			if loop then
+				loop = loop:Disconnect()
 			end
 		end
 	end)
 end)
-print("protection ran")
+Player.Chatted:Connect(function(Message)
+	if string.len(Message) <= 1 then return end
+	Message = string.split(Message, " ")
+	if #Message <= 1 then return end
+
+	local Command = string.lower(Message[1])
+	local Recipient = FindPlayer(string.lower(Message[2]))
+
+	if Command == ";protect" or Command == ";prot" then
+		if Recipient ~= nil then
+			loop = game:GetService('RunService').Stepped:Connect(function()
+				pcall(function()
+					local hrp = Char.HumanoidRootPart
+					local target = game.Players:FindFirstChild(Recipient).Character:WaitForChild("Humanoid")
+					hrp.CFrame = target.RootPart.CFrame + target.RootPart.CFrame.LookVector * 20
+
+					if target.Health <= 0 then loop = loop:Disconnect() end
+					if Player.Character:WaitForChild("Humanoid").Health <= 0 then loop = loop:Disconnect() end
+				end)
+			end)
+		end
+	elseif Command == ";unprotect" or Command == ";unprot" or Command == ";stop" then
+		if loop then
+			loop = loop:Disconnect()
+		end
+	end
+end)
